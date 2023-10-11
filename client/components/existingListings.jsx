7b782b07cb1b2989f36a911dfb220921569831d3
Dane Smith <daneswildlife@gmail.com>
2023-10-11 13:09:49 -0600

    new changes are causing build error - checkout to dev to compare code

--- client/components/existingListings.jsx
+++ client/components/existingListings.jsx
@@ -10,27 +10,23 @@ import {
   import { GlobalContext } from "../context/GlobalState.js";
   import { useLocation } from "react-router-dom";
 
-  // const context = useContext(GlobalContext);
-
-
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
-
-
 
 
   const ExistingListing = () => {
 
+    // let theme = createTheme();
+    // theme = responsiveFontSizes(theme); 
 
-    const location = useLocation();
-    const listingID = location.state.ListingID;
-    const context = useContext(GlobalContext);
-    const allListings = context.userBookings;
-    const userListing = {};
 
-    allListings.forEach((e) => {
-        if (listingID === e.ListingID) userListing = e
-    }); 
+    // const location = useLocation();
+    // const listingID = location.state.ListingID;
+    // const context = useContext(GlobalContext);
+    // const allListings = context.userListings;
+    // const userListing = {};
+
+    // allListings.forEach((e) => {
+    //     if (listingID === e.ListingID) userListing = e
+    // }); 
 
     
 
@@ -45,7 +41,7 @@ theme = responsiveFontSizes(theme);
                     <img src="https://media.istockphoto.com/id/1335929190/photo/car-parking-space.jpg?s=612x612&w=0&k=20&c=1Wekx1HZnQyA83y5kJElLyUCDoCCk8cLxHdSVjcS7U8=" width={'100%'} height={'100%'} />
                     </Box>
                 </Grid>
-                <Grid sx={{lg:6, height:'90vh', width: '50%', mt:3, mb: 8}}>
+                {/* <Grid sx={{lg:6, height:'90vh', width: '50%', mt:3, mb: 8}}>
                     <Box sx={{
                         border: 1,
                         height: '100%'
@@ -53,49 +49,49 @@ theme = responsiveFontSizes(theme);
                     >
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}>
-                        <Typography variant="h4" component="h1" marginTop={1} marginBottom={0} sx={{textAlign: "center"}}>{userListing.Name}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={1} marginBottom={0} sx={{textAlign: "center"}}>{userListing.name}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}>
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.Address}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.address}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}> 
                         <ThemeProvider theme={theme}>   
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.City}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.city}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}> 
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.State}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.state}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}> 
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.ZipCode}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.zipCode}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}> 
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.Hours}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.hours}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}> 
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.Rate}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.rate}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display="flex" alignContent={'left'} alignItems={'left'}>
                         <ThemeProvider theme={theme}> 
-                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.Description}</Typography>
+                        <Typography variant="h4" component="h1" marginTop={5} marginBottom={0} sx={{textAlign: "center"}}>{userListing.description}</Typography>
                         </ThemeProvider>
                     </Box>
                     <Box display='flex' justifyContent={'flex-end'}>
                         <Button variant="contained" sx={{mr:3, mt: 5}}>Edit Listing</Button><Button variant="contained" sx={{mr:3, mt:5}}>Save Changes</Button>
                     </Box>
                     </Box>
-                </Grid>
+                </Grid> */}
             </Grid>
         </Container>
     )
