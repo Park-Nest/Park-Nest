35ba40cacf5d24204d07f7491ae539293082ed2d
Dane Smith <daneswildlife@gmail.com>
2023-10-12 17:50:30 -0600

    revamped landing page with background image, new font, and framed text

--- client/pages/landingpage.jsx
+++ client/pages/landingpage.jsx
@@ -6,10 +6,11 @@ import {
     Container,
     Box,
   } from "@mui/material";
-import { blue } from '@mui/material/colors';
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/footer.jsx"
+import '../styles/styles.css'
+import { createTheme, ThemeProvider } from '@mui/material';
 
 
 
@@ -17,38 +18,73 @@ import Footer from "../components/footer.jsx"
 
     const navigate = useNavigate();
 
+    const boldTheme = createTheme({
+        typography: {
+          fontFamily: [
+            'Ubuntu',
+            'bold',
+          ].join(','),
+        },});
+
+    const theme = createTheme({
+        typography: {
+          fontFamily: [
+            'Ubuntu',
+            'regular',
+          ].join(','),
+        },});
+
     return (
     <div>
       <Paper sx={{backgroundImage: 'url("https://www.picturecorrect.com/wp-content/uploads/2015/11/traffic-night.jpg")',
       backgroundSize: '100%'}} >
+
+
+        <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '10%',
+        alignItems:  'left',
+        }}
+        >
+
+        <ThemeProvider theme={boldTheme}>
+          <Typography variant="h2" component='h2' marginTop="2%" marginLeft="2%" color="common.white">Park Nest</Typography>
+        </ThemeProvider>
+        
+        </Box>
+  
+
         <Container sx={{
         display: 'flex',
         flexDirection: 'column',
         width: '100%',
-        heigth: '100%',
         alignItems:  'center',
         height: '100vh',
         }}>
 
-        {/* <Box sx={{
-        display:"flex",
-        mt: '5%',
-        border: 1,
-        bgcolor: "white",
-        width: '100vh',
-        height: '40vh',
-        borderColor:  'common.black',
-        justifyContent:  'center',
-        alignItems:  'center',
+        <Box sx={{
+        display: 'flex',
+        mt: '20vh',
+        border: 4,
+        borderColor: "common.white",
+        width: '70%',
+        height: '20%',
+        padding: '3px',
+        justifyContent: 'center',
+        alignItems:'center',
+        bgcolor: 'common.black'
     }}
-        > */}
-       <Typography variant="h4" component='h2' marginTop='5%' padding="5%" color="common.white">
-        Welcome to Park Nest! </Typography>
-        <Typography variant="h4" component='h2' color="common.white">
-        <Link href='/login' color="common.white" variant="h4">Log In</Link> or <Link href='/signup' color="common.white">Sign Up</Link> to find your space today.
+        >
+
+        <ThemeProvider theme={theme}>
+        <Typography variant="h4" component='h2' color="common.white" marginTop="10%" marginBottom="10%">
+        <Link href='/login' color="common.white" variant="h4">Login</Link> or <Link href='/signup' color="common.white">Sign Up</Link> to find your space today.
         </Typography>
-{/*        
-        </Box> */}
+        </ThemeProvider>
+       
+        </Box>
         </Container>
         </Paper>
         <Footer/>
