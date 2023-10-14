df5099cf9ed78e9c6a8ca8d9662d5ec354fac910
Apiraam Selvabaskaran <apiraam96@gmail.com>
2023-10-14 14:41:52 -0400

    quick fix to handleLogout function in navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -27,11 +27,11 @@ const Navbar = () => {
 
   return (
     <nav className='navbar' >
-        <a href="/search" className="logo">PARK NEST</a>
-        <ul className="navLinks">
-          <a href="/profile">PROFILE</a>
-          <a href="/">LOGOUT</a>
-        </ul>
+      <a href="/search" className="logo">PARK NEST</a>
+      <ul className="navLinks">
+        <a href="/profile">PROFILE</a>
+        <a onClick={handleLogout}>LOGOUT</a>
+      </ul>
     </nav>
   );
 }
